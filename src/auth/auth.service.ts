39854b96bb2d7de496177384dd0ac4494c72489b
Ilya-Stafeev <stafeev-ilya@bk.ru>
2024-09-09 22:32:00 +0700

    I started implementing authorization and registration

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,7 +1,10 @@
-import { Injectable } from '@nestjs/common';
-import { InjectModel } from 'nestjs-typegoose';
-import { UserModel } from 'src/user/user.model';
+import { BadRequestException, Injectable, UnauthorizedException } from '@nestjs/common';
 import { ReturnModelType } from '@typegoose/typegoose';
+import { InjectModel } from 'nestjs-typegoose';
+import { hash, genSalt, compare } from 'bcryptjs'
+
+import { UserModel } from 'src/user/user.model';
+import { AuthDto } from './dto/auth.dto';
 
 @Injectable()
 export class AuthService {
@@ -9,8 +12,33 @@ export class AuthService {
     @InjectModel(UserModel) private readonly userModel: ReturnModelType<typeof UserModel>,
   ) {}
 
-  async register(dto: any) {
-    const newUser = new this.userModel(dto);
+  async login(dto: AuthDto) {
+    return this.validateUser(dto)
+  }
+
+  async register(dto: AuthDto) {
+    const oldUser = await this.userModel.findOne({email: dto.email})
+    if (oldUser)
+        throw new BadRequestException (
+            'User with this email is already in the system'
+        )
+
+        const salt = await genSalt(10)
+
+    const newUser = new this.userModel({
+        email: dto.email,
+        password: await hash(dto.password, salt)
+    });
     return newUser.save();
+  }
+
+  async validateUser(dto: AuthDto):Promise<UserModel> {
+    const user = await this.userModel.findOne({email: dto.email})
+    if(!user) throw new UnauthorizedException('User not found')
+
+    const isValidPassword = await compare(dto.password, user.password)
+    if(!isValidPassword) throw new UnauthorizedException('Invalid password')
+
+    return user
   }
 }
