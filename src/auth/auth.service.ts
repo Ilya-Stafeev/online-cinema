da9b5b76bf60b79d6b173cfeb8217e672e731d5e
Ilya-Stafeev <stafeev-ilya@bk.ru>
2024-09-12 22:32:50 +0700

    Completed the authorization

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -6,6 +6,7 @@ import { JwtService } from '@nestjs/jwt';
 
 import { UserModel } from 'src/user/user.model';
 import { AuthDto } from './dto/auth.dto';
+import { RefreshTokenDto } from './dto/refreshToken.dto';
 
 @Injectable()
 export class AuthService {
@@ -16,6 +17,22 @@ export class AuthService {
 
   async login(dto: AuthDto) {
     const user = await this.validateUser(dto)
+
+    const tokens = await this.issueTokenPair(String(user._id))
+
+    return {
+      user: this.returnUserFields(user),
+      ...tokens
+    }
+  }
+
+  async getNewTokens({ refreshToken }: RefreshTokenDto) {
+    if (!refreshToken) throw new UnauthorizedException('Please sign in')
+
+    const result = await this.jwtService.verifyAsync(refreshToken)
+    if (!result) throw new UnauthorizedException('Invalid token or expired!')
+
+    const user = await this.userModel.findById(result._id)
 
     const tokens = await this.issueTokenPair(String(user._id))
 
